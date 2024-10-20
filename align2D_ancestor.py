d6543f0f40ab064d034398661120aad84cc2045b
dsmoltedo <dominique.schwend@uc.cl>
2024-10-20 11:46:16 -0300

    Update align2D_ancestor.py

--- align2D_ancestor.py
+++ align2D_ancestor.py
@@ -2,9 +2,9 @@ from modeller import *
 
 env = environ()
 aln = alignment(env)
-mdl = model(env, file='PROTEIN', model_segment=('FIRST:A','LAST:A'))
-aln.append_model(mdl, align_codes='6eqeA', atom_files='6eqe.pdb')
-aln.append(file='target.fasta', align_codes='target', alignment_format='FASTA')
+mdl = model(env, file='1uwq', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='1uwq', atom_files='1uwq.pdb')
+aln.append(file='node9.fasta', align_codes='node9', alignment_format='FASTA')
 aln.align2d()
 aln.write(file='aligned_ancestor.fasta', alignment_format='FASTA')
 aln.write(file='aligned_ancestor.ali', alignment_format='PIR')
