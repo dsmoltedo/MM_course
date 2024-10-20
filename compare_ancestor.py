99133aa85a8269c0d562c4680fd55fe55078f4ad
dsmoltedo <dominique.schwend@uc.cl>
2024-10-20 08:40:54 -0300

    Create compare_ancestor.py

---
+++ compare_ancestor.py
@@ -0,0 +1,13 @@
+from modeller import *
+
+env = environ()
+aln = alignment(env)
+for (pdb, chain) in (('1qvb', 'A'), ('3wdp', 'P'), ('4ha4', 'A'),
+                     ('7uz1', 'A'), ('5i3d', 'C'),):
+    m = model(env, file=pdb, model_segment=('FIRST:'+chain, 'LAST:'+chain))
+    aln.append_model(m, atom_files=pdb, align_codes=pdb+chain)
+aln.malign()
+aln.malign3d()
+aln.compare_structures()
+aln.id_table(matrix_file='family.mat')
+env.dendrogram(matrix_file='family.mat', cluster_cut=-1.0)
